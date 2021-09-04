5ceda1c51a5c82966866bf6d4f3c267add80024e
Joel Nwamba <joel.elyon.nwamba@gmail.com>
2021-09-04 19:30:41 -0400

    add function

--- js/script.js
+++ js/script.js
@@ -3,12 +3,19 @@ const emojiContainer = document.getElementById("emoji-container");
 const inputField = document.getElementById("input-field");
 const addBtn = document.getElementById("add-btn");
 
-for(let i = 0; i < myEmojis.length; i++) {
-    const span = document.createElement("span");
-    span.textContent = myEmojis[i];
-    emojiContainer.append(span)
+function renderEmoji(){
+    for(let i = 0; i < myEmojis.length; i++) {
+        const span = document.createElement("span");
+        span.textContent = myEmojis[i];
+        emojiContainer.append(span)
+    }
 }
 
 addBtn.addEventListener("click", function() {
-    inputField.value; 
+    if(inputField.value) {
+        myEmojis.push(inputField.value);
+        console.log(myEmojis)
+        inputField.value = "";
+        renderEmoji()
+    }
 })

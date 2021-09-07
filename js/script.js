5403549415999910c1abf841d5908459aaee89a7
Joel Nwamba <joel.elyon.nwamba@gmail.com>
2021-09-06 20:35:27 -0400

    Added interaction for removing emoji and adding emoji at the front

--- js/script.js
+++ js/script.js
@@ -2,14 +2,18 @@ const myEmojis = ["🏀", "🏋🏾‍♂️", "🙏🏾"];
 const emojiContainer = document.getElementById("emoji-container");
 const inputField = document.getElementById("input-field");
 const addBtn = document.getElementById("add-btn");
+const removeBtn = document.getElementById("remove-btn");
+const frontBtn = document.getElementById("front-btn");
 
-function renderEmoji(){
+function renderEmojis(){
     for(let i = 0; i < myEmojis.length; i++) {
         const span = document.createElement("span");
         span.textContent = myEmojis[i];
-        emojiContainer.append(span)
+        emojiContainer.append(span);
     }
 }
+
+renderEmojis();
 
 addBtn.addEventListener("click", function() {
     if(inputField.value) {
@@ -17,6 +21,22 @@ addBtn.addEventListener("click", function() {
         inputField.value = "";
         console.log(myEmojis);
         emojiContainer.textContent = "";
-        renderEmoji()
+        renderEmojis()
+    }
+});
+
+removeBtn.addEventListener("click", function() {
+    myEmojis.pop();
+    emojiContainer.textContent = "";
+    renderEmojis();
+});
+
+frontBtn.addEventListener("click", function() {
+    if(inputField.value) {
+        myEmojis.unshift(inputField.value);
+        inputField.value = "";
+        console.log(myEmojis);
+        emojiContainer.textContent = "";
+        renderEmojis()
     }
 })
